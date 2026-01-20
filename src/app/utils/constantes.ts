ae25d7a0fd572458f2aba3f3cd9c3b9b3cc5eb91
JeronimoCortez <jeronimofcortez3@gmail.com>
2026-01-20 12:47:24 -0300

    Nuevo diseño

---
+++ src/app/utils/constantes.ts
@@ -0,0 +1,164 @@
+export const coachName = "Santiago Rama"
+
+// export const coachFullName = "Santiago Rama"
+
+export const pixelId = "877786941359778"
+
+export const calendlyBaseUrl = "https://calendly.com/santiagoscf/calendario-sesion-gratuita-consultoria"
+
+export const waNumber = "+5491180301000";
+
+export const ALT_IMG_GENERIC = "Santiago Rama - FIT"
+
+// Calendario: helpers
+export const titleCalender = "Plan para recomponer hormonas y músculo";
+export const descriptionCalender = `Reunión 1:1 para alinear objetivos y plan.\nEnlace: MEETING_URL\nTraer dudas y objetivos claros.`;
+export const locationCalender = "Online (se compartirá enlace)";
+
+// VSL
+export const idVsl = "panda-636f6d70-2132-406a-8742-7f9c892a38c0";
+export const srcVsl = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com.br/embed/?v=636f6d70-2132-406a-8742-7f9c892a38c0";
+
+// Thankyou
+export const idThankyou = "panda-c2965b0d-2013-4ae6-b3ee-adffba85fade";
+export const srcThankyou = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=c2965b0d-2013-4ae6-b3ee-adffba85fade";
+
+// Make o n8n
+// export const sendForm = "https://n8n.srv953925.hstgr.cloud/webhook/19ffc8bc-f934-4d70-829a-c914d08509fc"
+
+export const VIDEO_TESTIMONIALS = [
+    {
+        cambio: "108 a 88 kg en 6 meses (-20 kg)",
+        video: "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=a0506554-1f1d-4678-8ffb-eb6afc047bce",
+        titulo: "Los resultados de Marc están a la vista",
+        story: "Hay mucha gente que que me ha notado el cambio y me lo han dicho.",
+        nombre: "Marc",
+        dato: "Cataluña, España",
+    },
+    {
+        cambio: "98 a 86 kg 3 meses (-12 kg)",
+        video: "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=a8a76d1e-173c-478a-8daa-aa1ff9e7768a",
+        titulo: "Hernan volvió a confiar en sí mismo",
+        story: "Sentirse bien animicamente... emocionalmente te ayuda mucho.",
+        nombre: "Hernan",
+        dato: "EEUU",
+    },
+    {
+        cambio: "102 a 90 kg en 4 meses (-12 kg)",
+        video: "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=74190c9d-d6e3-47b1-bbb8-4138f8646eac",
+        titulo: "Mario logró resultados sostenibles",
+        story: "El dinero mejor invertido que que he tenido en mucho tiempo.",
+        nombre: "Mario",
+        dato: "Panamá",
+    },
+    {
+        cambio: "96 a 86 kg en 5 meses (-10 kg)",
+        video: "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=b6ca9cd8-e908-4018-80a9-d7d2e01f23c7",
+        titulo: "Diego consiguió resultados que realmente se sienten",
+        story: "Hoy no estoy haciendo un esfuerzo para no subir.",
+        nombre: "Diego",
+        dato: "Cordoba, Argentina",
+    },
+    {
+        cambio: "89 a 83 kg en 3 meses (-16 kg)",
+        video: "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=be556864-7e64-4b15-9ab7-75c1fd1c9689",
+        titulo: "Héctor transformó su rutina con ajustes imperceptibles",
+        story: "Sigo comiendo lo mismo que sigo comiendo en la regaladora, digo, ¡tán fácil!... Cambié lo imperceptible.",
+        nombre: "Hector",
+        dato: "Monterrey, México",
+    },
+];
+
+export const TESTIMONIALS = [
+    {
+        weight: "-5kg en 3 meses",
+        img: "/images/testimonios/placeholder.webp",
+    },
+    {
+        weight: "-5kg en 3 meses",
+        img: "/images/testimonios/placeholder.webp",
+    },
+    {
+        weight: "-5kg en 10 meses",
+        img: "/images/testimonios/placeholder.webp",
+    },
+    {
+        weight: "-5kg en 3 meses",
+        img: "/images/testimonios/placeholder.webp",
+    },
+];
+
+// Imagen con texto en testimonios page
+export const MORE_CHANGES_IMG = '/images/testimonios/placeholder.webp';
+
+// Hacemos una constante aparte para modificar puntualmente que imagenes queremos en THANKYOU
+export const TESTIMONIALS_THANKYOU_IMG = [
+    { txt: "-17 KG en 3 Meses", img: "/images/testimonios/placeholder.webp" },
+    { txt: "-4 KG en 1 Mes", img: "/images/testimonios/placeholder.webp" },
+    { txt: "-5,5 KG en 1 Meses", img: "/images/testimonios/placeholder.webp" },
+]
+
+// El testimonio que se habilita despues de ver el vsl principal de page
+export const TESTIMONIALS_VIDEO_PAGE = [
+    {
+        video:
+            "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=a0506554-1f1d-4678-8ffb-eb6afc047bce",
+        titulo: "Mateo bajo 17 KG en SOLO 3 meses",
+        story:
+            "Antes me miraba de reojo y me veía espantoso... Son 90 días que me cambiaron la vida. Es un cambio gigante no solo para mi cuerpo, sino también para mi mentalidad.",
+        nombre: "Mateo Falco",
+        dato: "Buenos Aires, Argentina",
+    },
+];
+
+
+export const CALENDLY_TITLE_PART1 =
+    "Nuestros clientes bajan entre 8 y 20 kg de grasa corporal en 3 a 6 meses.";
+
+export const CALENDLY_SPAN = "Vos también podés";
+
+export const CALENDLY_TITLE_PART2 =
+    "Agenda una llamada y vamos a hablar de:";
+
+export const hotjarID = "<<LLENAR>>"
+
+// Que incluye el metodo landing
+export const METHOD_INCLUDES = [
+    {
+        img: "./images/medical_mobile_app.svg",
+        title: `App Mobile "TestoBoost" Todo en uno`
+    },
+    // {
+    //     img: "./images/hand_with_a_pill.svg",
+    //     title: `Suplementación Natural Diagnosticada por el Dr. Alan Inman`
+    // }, 
+    // {
+    //     img: "./images/frying_pan.svg",
+    //     title: `20 Viandas Saludables Mensuales recetadas por la Lic. Ana buteler`
+    // }, 
+    // {
+    //     img: "./images/gym.svg",
+    //     title: `Sistema Personalizado de Alimentación y Entrenamiento`
+    // },
+    {
+        img: "./images/stethoscope.svg",
+        title: `Diagnóstico y Monitoreo Hormonal`
+    }, 
+    {
+        img: "./images/classroom.svg",
+        title: `Clases de Motivación y Liderazgo`
+    }, 
+    {
+        img: "./images/test_passed.svg",
+        title: `Tacker de Hábitos`
+    }, 
+    {
+        img: "./images/batch_assign.svg",
+        title: `Comunidad Exclusiva`
+    }, 
+    {
+        img: "./images/today.svg",
+        title: `Seguimiento y Acompañamiento Diario`
+    },
+
+]
